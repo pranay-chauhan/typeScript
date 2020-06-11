8d85d67d845b109330adbf70ab9a10d892714b14
pranay <pranay.s.chauhan@gmail.com>
2020-06-11 19:14:17 +0530

    first file added in typescript

--- a1.js
+++ a1.js
@@ -1,6 +1,4 @@
 //Typescript is superset of Javascript
 //Browser doesn't understand the typescript code so we should convert .ts to .js using transpiler,it will convert your .ts(typescript file) to js file in the following manner
-
 //tsc filename
-
 console.log('Hello Typescript');
