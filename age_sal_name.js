be5a43e02176d5398bba35f8eb5be4860d8eae3d
pranay <pranay.s.chauhan@gmail.com>
2020-06-11 19:24:39 +0530

    basic in typescript

---
+++ age_sal_name.js
@@ -0,0 +1,6 @@
+var name1 = 'Mohan';
+var dob = 1987;
+var sal = 34000;
+console.log("The name of person is " + name1);
+console.log("The age of person is " + (2020 - dob));
+console.log("The salary of person is " + sal);
---
+++ age_sal_name.ts
@@ -0,0 +1,7 @@
+let name1:string = 'Mohan';
+let dob:number = 1987;
+let sal:number = 34000
+
+console.log(`The name of person is ${name1}`);
+console.log(`The age of person is ${2020-dob}`);
+console.log(`The salary of person is ${sal}`);
